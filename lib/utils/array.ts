c27547125eec6ed0f583c754a83355970394eeb7
Richard Hubbard <richard.hubbard@armadry.co.uk>
2022-12-14 12:04:44 +0000

    Some array utilities

---
+++ lib/utils/array.ts
@@ -0,0 +1,11 @@
+export function chunk<T>(a: T[], size: number): T[][] {
+  return a.flatMap((_, i, a) => (i % size) ? [] : [a.slice(i, i + size)]);
+}
+
+export function sumOf(a: number[], map: (i: any) => number = i => i): number {
+  return a.reduce((a, b) => map(a) + map(b));
+}
+
+export function productOf(a: number[], map: (i: any) => number = i => i): number {
+  return a.reduce((a, b) => map(a) * map(b));
+}
--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,4 @@
+export * from './array';
 export * from './combi';
 export * from './data';
 export * from './grid';
