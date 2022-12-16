7a79edf16c8f5afac9356e883402edb073418357
Richard Hubbard <richard.hubbard@armadry.co.uk>
2022-12-16 23:31:16 +0000

    Day 16 solution, part 2 *very* slow

--- lib/utils/array.ts
+++ lib/utils/array.ts
@@ -16,3 +16,18 @@ export function sumOf(a: number[], map: (i: any) => number = i => i): number {
 export function productOf(a: number[], map: (i: any) => number = i => i): number {
   return a.reduce((a, b) => map(a) * map(b));
 }
+
+export function findBest<T>(a: T[], scoreFn: (i: T) => number): [T, number] {
+  return <[T, number]>a.reduce(
+    (t: [T | undefined, number], current) => {
+      const [best, max] = t;
+
+      const score = scoreFn(current);
+      if (!best || (score > max)) {
+        return [current, score];
+      }
+      return [best, max];
+    },
+    [undefined, Number.MIN_SAFE_INTEGER]
+  )
+}
