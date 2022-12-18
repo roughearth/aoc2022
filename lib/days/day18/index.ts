4b4b07ce6d9e5f706d50585fc017debd7bae6654
Richard Hubbard <richard.hubbard@armadry.co.uk>
2022-12-18 11:12:02 +0000

    Day 18 part 1 solution

--- lib/days/day18/index.ts
+++ lib/days/day18/index.ts
@@ -1,17 +1,36 @@
-import { input } from './input';
-import { cleanAndParse } from '../../utils';
+import { eg1, input } from './input';
+import { cleanAndParse, Coordinate, CoordinateRange, coordinates, getIntKey, inRange, orthogonalNeighbours } from '../../utils';
 import { Day } from '..';
 
 export const meta: Day['meta'] = {};
 
-export function part1() {
-  const data = cleanAndParse(input, Number);
+function toNeighbourEntries(src: string): [string, string[]] {
+  const coords = src.split(",").map(Number);
 
-  return data.length;
+  const neighbourCoords = Array.from(orthogonalNeighbours(coords));
+
+  return [src, neighbourCoords.map(c => c.join(","))];
 }
 
-export function part2() {
-  const data = cleanAndParse(input, Number);
 
-  return data.length;
+export function part1() {
+  const entries = cleanAndParse(input, toNeighbourEntries);
+
+  const neighbourMap = new Map(entries);
+  let found = 0;
+
+  for (const [cube, neighbours] of neighbourMap) {
+    for (const neighbour of neighbours) {
+      if (neighbourMap.has(neighbour)) {
+        found++;
+      }
+    }
+  }
+
+  return (neighbourMap.size * 6) - found;
+}
+
+
+export function part2() {
+  return -999;
 }
