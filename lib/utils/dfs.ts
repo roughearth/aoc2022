75eebebed667f69437c1b1450a1f21eca00a76ac
Richard Hubbard <richard.hubbard@armadry.co.uk>
2022-12-22 01:28:45 +0000

    Some stack and Set 'optimisations'

--- lib/utils/dfs.ts
+++ lib/utils/dfs.ts
@@ -4,15 +4,94 @@ export type DFS<T> = {
   leafs: () => Generator<T, void, unknown>
 };
 
+
+const fooGenerator: () => Generator<number, void, undefined> = function* () {
+  for (let i = 1; i <= 10; i++) {
+    yield i;
+  }
+}
+
+const fooIterator: () => Iterator<number> = function () {
+  let i = 1;
+  return {
+    next() {
+      return {
+        value: i++,
+        done: i <= 10
+      };
+    }
+  };
+}
+
+const fooIterable: () => Iterable<number> = function () {
+  let i = 1;
+  return {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          return {
+            value: i++,
+            done: i <= 10
+          };
+        }
+      };
+    }
+  };
+}
+
+const fooGeneratorArray: () => number[] = function () {
+  return Array.from(fooGenerator());
+}
+
+const fooIterableArray: () => number[] = function () {
+  return Array.from(fooIterable());
+}
+
+export function Stack<T>(start?: T) {
+  type Top = [T, Top] | undefined;
+  let top: Top;
+
+  const _this = {
+    push(item: T) {
+      top = [item, top];
+    },
+    pop() {
+      let item;
+      if (top) {
+        [item, top] = top;
+      }
+      return item;
+    },
+    hasMore() {
+      return Boolean(top);
+    }
+  };
+
+  if (start) {
+    _this.push(start);
+  }
+
+  return _this;
+}
+export type Stack<T> = ReturnType<typeof Stack<T>>;
+
 export function dfsFrom<T>(
   startNode: T,
-  edgeFn: (n: T) => T[] | undefined
+  edgeFn: (n: T) => T[] | undefined,
+  {
+    setConstructor = () => (new Set<T>())
+  }: {
+    setConstructor?: () => {
+      has: (i: T) => boolean,
+      add: (i: T) => void
+    }
+  } = {}
 ): DFS<T> {
   function* nodes(): Generator<[T, number | undefined], void, unknown> {
-    const stack: T[] = [startNode];
-    const visitedNodes = new Set<T>();
+    const stack: Stack<T> = Stack(startNode);
+    const visitedNodes = setConstructor();
 
-    while (stack.length) {
+    while (stack.hasMore()) {
       const next = stack.pop()!;
 
       if (!visitedNodes.has(next)) {
